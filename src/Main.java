5486029cc5a446d78b3c080b4ca2c9647bead127
Bartele12 <kamiro1234567890@wp.pl>
2025-04-04 08:29:15 +0200

    Pisze to na githubie
    zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zadanie");
+        //pisze to na github
+        System.out.print("Hello world");
 
     }
 }
